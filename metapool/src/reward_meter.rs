37ec40b4c3b7b0ff6713503541fb1289eb52308b
luciotato <luciotato@gmail.com>
2021-04-15 22:55:32 -0300

    restore reward meter storage model

--- metapool/src/reward_meter.rs
+++ metapool/src/reward_meter.rs
@@ -10,70 +10,53 @@ pub use crate::utils::*;
 pub struct RewardMeter {
   ///added with staking
   ///subtracted on unstaking. WARN: Since unstaking can include rewards, delta_staked *CAN BECOME NEGATIVE*
-  pub delta: u128,
-  pub negative: bool,
-  /// (pct: 100 => x1, 200 => x2)
-  pub last_multiplier_pct: u16,
+  pub delta_staked: i128, //i128 changing this requires accounts migration
+  pub last_multiplier_pct: u16,// (pct: 100 => x1, 200 => x2)
 }
 
 impl Default for RewardMeter {
   fn default() -> Self {
     Self {
-      delta: 0,
-      negative: false,
+      delta_staked: 0,
       last_multiplier_pct: 100,
     }
   }
 }
 
 impl RewardMeter {
+  fn open(&self)->(bool,u128){
+    if self.delta_staked<0 {
+      (true, (-self.delta_staked) as u128)
+    }
+    else  {
+      (false, self.delta_staked as u128)
+    }
+  }
   /// compute rewards received (extra after stake/unstake)
   /// multiplied by last_multiplier_pct%
   pub fn compute_rewards(&self, valued_shares: u128) -> u128 {
-    if self.negative || self.delta >= valued_shares {
+    let (negative,delta) = self.open();
+    if negative || delta >= valued_shares {
       return 0; //withdrew all or no positive difference => no rewards, fast exit
     }
-    let rewards_by_difference = valued_shares - self.delta;
+    let rewards_by_difference = valued_shares - delta;
     return apply_multiplier(rewards_by_difference, self.last_multiplier_pct);
   }
   ///register a stake (to be able to compute rewards later)
   pub fn stake(&mut self, value: u128) {
-    if self.negative {
-      if value >= self.delta {
-        //crosses 0
-        self.delta = value - self.delta;
-        self.negative = false;
-      } else {
-        //negative, value<delta
-        self.delta -= value;
-      }
-    }
-    else {
-      self.delta += value;
-    }
+    assert!(value<=std::i128::MAX as u128);
+    self.delta_staked += value as i128;
   }
   ///register a unstake (to be able to compute rewards later)
   pub fn unstake(&mut self, value: u128) {
-    if self.negative {
-      //already negative
-      self.delta += value;
-    } else {
-      //positive
-      if value > self.delta {
-        //crosses 0
-        self.delta = value - self.delta;
-        self.negative = true;
-      } else {
-        //positive, delta>=value
-        self.delta -= value;
-      }
-    }
+    assert!(value<=std::i128::MAX as u128);
+    self.delta_staked -= value as i128;
   }
 
   #[inline]
   pub fn reset(&mut self, valued_shares: u128) {
-    self.delta = valued_shares; // reset meter to Zero
-    self.negative = false;
+    assert!(valued_shares<=std::i128::MAX as u128);
+    self.delta_staked = valued_shares as i128; // reset meter to Zero difference
   }
   /// compute realized rewards & reset
   /// compute rewards received (extra after stake/unstake) multiplied by last_multiplier_pct%
