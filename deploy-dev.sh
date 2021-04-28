1bf382fd21cdded2f5c4a1921c771db0b6d6bd5f
luciotato <luciotato@gmail.com>
2021-04-28 18:46:31 -0300

    tokenomics

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,6 +3,7 @@ NETWORK=testnet
 OWNER=lucio.$NETWORK
 MASTER_ACC=pool.$NETWORK
 CONTRACT_ACC=devmeta.$MASTER_ACC
+GOV_TOKEN=token.devmeta.$MASTER_ACC
 
 #set meta-cli target
 meta --cliconf -c $CONTRACT_ACC -acc $OWNER
@@ -10,14 +11,14 @@ meta --cliconf -c $CONTRACT_ACC -acc $OWNER
 export NODE_ENV=$NETWORK
 
 ## delete acc
-echo "Delete $CONTRACT_ACC? are you sure? Ctrl-C to cancel"
-read input
-near delete $CONTRACT_ACC $MASTER_ACC
+#echo "Delete $CONTRACT_ACC? are you sure? Ctrl-C to cancel"
+#read input
+#near delete $CONTRACT_ACC $MASTER_ACC
 #near create-account $CONTRACT_ACC --masterAccount $MASTER_ACC
-#meta deploy ./res/metapool.2021-04-08.07:27:12.wasm
-#meta new { owner_account_id:$OWNER, treasury_account_id:treasury.$CONTRACT_ACC, operator_account_id:operator.$CONTRACT_ACC} --accountId $MASTER_ACC
+#meta deploy ./res/metapool.wasm
+meta new { owner_account_id:$OWNER, treasury_account_id:treasury$OPERATOR_ACC_SUFFIX, operator_account_id:operator$OPERATOR_ACC_SUFFIX, meta_token_account_id:$GOV_TOKEN } --accountId $MASTER_ACC
 ## set params@meta set_params
-#meta default_pools_testnet
+meta default_pools_testnet
 
 
 ## redeploy code only
