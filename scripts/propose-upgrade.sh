e07345c0de1cacc131d877e6120f74fbddc0aa36
the-preem-palver <the.preem.palver@protonmail.com>
2022-06-29 15:16:23 -0300

    store wasm backup

--- scripts/propose-upgrade.sh
+++ scripts/propose-upgrade.sh
@@ -1,5 +1,6 @@
+CONTRACT_ACC=meta-pool.near
 set +ex
-meta-util dao propose upgrade meta-pool.near res/metapool.wasm
-mkdir -p res/mainnet
-cp res/metapool.wasm res/mainnet/metapool.`date +%F.%T`.wasm
+meta-util dao propose upgrade $CONTRACT_ACC res/metapool.wasm
+mkdir -p res/mainnet/metapool
+cp res/metapool.wasm res/mainnet/metapool/$CONTRACT_ACC.`date +%F.%T`.wasm
 date +%F.%T
